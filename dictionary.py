a5abea9a4872bdcb3582155f83452001b431423f
TalandBotirjon <85695336+TalandBotirjon@users.noreply.github.com>
2021-06-23 22:55:45 +0500

    dictionary in python

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+
+
+
+Users = {
+    'first_name': "Botirjon",
+    'last_name': "Ergashov",
+    'age': 22,
+    'hobby':'study'
+}
+
+
+print(Users['first_name'])
+
+for key, value in Users.items():
+    
+    print(f"{value}")
+    
+    
